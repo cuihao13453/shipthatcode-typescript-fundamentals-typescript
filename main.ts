3c5c3221e2d6642e0d0d1436a86fbe183931edb9
cuihao <ch13453@sina.com>
2026-08-11 16:19:00 +0800

    lesson 06

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 // Print the greeting below.
-interface User {
-  name: string;
-  age: number;
-}
+const n: number = Number(require("fs").readFileSync(0, "utf-8").trim());
 
-const lines: string[] = require("fs")
-  .readFileSync(0, "utf-8")
-  .trim()
-  .split("\n");
-const u: User = { name: lines[0], age: Number(lines[1]) };
-console.log(`Hi, ${u.name}! You are ${u.age} years old.`);
+if (n % 15 === 0) {
+  console.log("FizzBuzz");
+} else if (n % 3 === 0) {
+  console.log("Fizz");
+} else if (n % 5 === 0) {
+  console.log("Buzz");
+} else {
+  console.log(n);
+}
