ad6ac14b7023da0315bd39cf1222dff56165b67c
cuihao <ch13453@sina.com>
2026-08-25 10:02:31 +0800

    lesson 10

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
-function last<T>(arr: T[]): T | undefined {
-  // Return the last element, or undefined.
-  return arr[arr.length - 1];
-}
+const nums: number[] = require("fs")
+  .readFileSync(0, "utf-8")
+  .trim()
+  .split(" ")
+  .map(Number);
 
-const line: string = require("fs").readFileSync(0, "utf-8").trim();
-const nums: number[] = line.length === 0 ? [] : line.split(" ").map(Number);
-const v = last(nums);
-if (v !== undefined) console.log(v);
+const max = nums.reduce((max, n) => (n > max ? n : max), nums[0]);
+console.log(max);
